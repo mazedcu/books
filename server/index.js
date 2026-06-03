c32dee10f84e83758dd4767847a34cd051111068
Mohammad Hasan Mazed <mazedbooks@gmail.com>
2026-06-03 19:38:05 +0600

    Increase Express JSON limit to 100mb

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ const GOOGLE_CLIENT_ID = process.env.VITE_GOOGLE_CLIENT_ID || '1067207455823-pla
 const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // Set up static serving for uploads
 const uploadsDir = path.join(__dirname, 'uploads');
